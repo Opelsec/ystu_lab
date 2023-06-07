dd16c45fd01a6f6c044727fa36e6a24250d69b54
Ñåì¸íîâ Àðò¸ì <Semenov.artemik@mail.ru>
2023-06-07 13:32:06 +0300

    Äåëåíèå íà íîëü

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,7 +6,48 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a;
+            double b;
+            double total;
+            char oper;
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+            if (oper == '+')
+            {
+                total = a + b;
+                Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+            }
+
+            else if (oper == '-')
+            {
+                total = a - b;
+                Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+            }
+
+            else if (oper == '*')
+            {
+                total = a * b;
+                Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+            }
+
+            else if (oper == '/')
+            {
+                total = a / b;
+                if (b==0)
+                    Console.WriteLine("Делить на 0 НЕЛЬЗЯ!");
+                else
+                Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор.");
+            }
         }
     }
 }
